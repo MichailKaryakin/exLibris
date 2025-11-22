e69b528ea211769d29558d27bdbac7d3b467c4a0
Michail <michai20karyakin@gmail.com>
2025-11-22 23:11:20 +0500

    do the initial setup

---
+++ src/main/java/org/example/exlibris/ExLibrisApplication.java
@@ -0,0 +1,13 @@
+package org.example.exlibris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExLibrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExLibrisApplication.class, args);
+    }
+
+}
